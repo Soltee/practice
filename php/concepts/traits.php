75680f29fa2110d5a7e1582727d2852f53f803a9
Prabin Gurung <37467008+Soltee@users.noreply.github.com>
2022-02-23 07:33:01 +0545

    traits in php

--- php/concepts/traits.php
+++ php/concepts/traits.php
@@ -5,24 +5,61 @@
 echo "<b>Traits<br/></b>";
 echo "Have constructor, use many Traits, have methods and Data Fields. <br/>";
 echo "Class can use many traits (use). <br/>";
-echo "<br/><br/>";
-echo "Cannot implement interfaces, extend Classes,  <br/><br/>";
-echo "No Abstract Methods. <br/><br/>";
+
+echo "Cannot implement interfaces, extend Classes, <br/>";
+echo "No Abstract Methods.<br/>";
 echo "Cannot instantiate Objects. <br/><br/>";
 
 /**
   * summary
   */
- trait Name
- {
-     /**
-      * summary
-      */
-     public function name()
-     {
-         
-     }
- } 
+trait A
+{  
+  public $string  = 'Hello ';
+  public function __construct()
+  {
+        
+    echo "Trait A";
+  }
+
+} 
+
+/**
+ * summary
+ */
+trait B
+{
+  use A;
+
+  public function __construct()
+  {
+    echo "Trait B.<br />";
+  }
+  
+}
+
+trait C
+{
+
+  use A, B;
+
+  public function __construct()
+  {
+    echo "Trait C.<br />";
+    $this->string .= "Trait A.<br />";
+
+  }
+
+}
+
+
+class D
+{
+  use C;
+}
+
+$c = new D();
+echo $c->string;
 
 
 
